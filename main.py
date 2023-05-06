d132a1edefa288433e0bc3540ebff4ff83ed27de
liangbinjie <mailandresliang@gmail.com>
2023-05-05 23:51:37 -0600

    popup
    Se agrego popups a los botones de los diferentes modulos
    Se hizo un cambio al archivo main, de manera que no use clases

--- main.py
+++ main.py
@@ -1,73 +1,80 @@
 from tkinter import *
-from toolbar import vMantenimiento, vReportes
-
-# https://zetcode.com/tkinter/menustoolbars/
-class MainFrame(Frame):
-
-    def __init__(self):
-        super().__init__()
-
-        self.ventanaPrincipal()
+from toolbar import *
 
 
-    def ventanaPrincipal(self):
+master = Tk()
+master.geometry("880x620")
+master.title("TecnoPETS - Tu Veterinaria de confianza")
 
-        self.master.title("TecnoPETS - Tu Veterinaria de confianza")
+menubar = Menu(master)
+master.config(menu=menubar)
 
-        menubar = Menu(self.master)
-        self.master.config(menu=menubar)
 
-        
-        # definimos la opcion de mantenimiento
-        mantenimiento = vMantenimiento(menubar) # la llamamos de la funcion de mantenimiento
+# definimos la opcion de mantenimiento
+mantenimiento = vMantenimiento(menubar) # la llamamos de la funcion de mantenimiento
 
-        # Definimos la opcion de reportes
-        reportes = vReportes(menubar)
-        
-        informacion = Menu(menubar)
-        informacion.add_cascade(label="Desarrollado por:")
-        informacion.add_cascade(label="- Binjie Liang")
-        informacion.add_cascade(label="- Aaron Moncada")
+# Definimos la opcion de reportes
+reportes = vReportes(menubar)
+
+informacion = Menu(menubar)
+informacion.add_cascade(label="Desarrollado por:")
+informacion.add_cascade(label="- Binjie Liang")
+informacion.add_cascade(label="- Aaron Moncada")
 
 
 
-        menubar.add_cascade(label="Mantenimiento", underline=0, menu=mantenimiento)
-        menubar.add_cascade(label="Reportes", underline=0, menu=reportes)
-        menubar.add_cascade(label="Informacion", underline=0, menu=informacion)
-        menubar.add_cascade(label="Contacto", underline=0)
+menubar.add_cascade(label="Mantenimiento", underline=0, menu=mantenimiento)
+menubar.add_cascade(label="Reportes", underline=0, menu=reportes)
+menubar.add_cascade(label="Informacion", underline=0, menu=informacion)
+menubar.add_cascade(label="Contacto", underline=0)
 
 
-        # ********** SECCION DE REDES SOCIALES Y LOGO ****************
-        self.facebook = PhotoImage(file='resources/facebook.png')
-        self.instagram = PhotoImage(file='resources/instagram.png')
-        self.twitter = PhotoImage(file="resources/twitter.png")
-        self.logo = PhotoImage(file='resources/LOGO.png')
+# ********** SECCION DE REDES SOCIALES Y LOGO ****************
+facebook = PhotoImage(file='resources/facebook.png')
+instagram = PhotoImage(file='resources/instagram.png')
+twitter = PhotoImage(file="resources/twitter.png")
+logo = PhotoImage(file='resources/LOGO.png')
 
-        Label(self.master, image=self.facebook).place(x=30, y=550)
-        Label(self.master, image=self.instagram).place(x=90, y=550)
-        Label(self.master, image=self.twitter).place(x=150, y=550)
-        Label(self.master, image=self.logo).place(x=560, y=550)
+Label(master, image=facebook).place(x=30, y=550)
+Label(master, image=instagram).place(x=90, y=550)
+Label(master, image=twitter).place(x=150, y=550)
+Label(master, image=logo).place(x=560, y=550)
+
+# ********* POPUPS ***********
+popup = Menu(master, tearoff=0)
+popup.add_command(label="Insertar")
+popup.add_command(label="Consultar")
+popup.add_command(label="Modificar")
+popup.add_command(label="Eliminar")
+
+def popupm(button, modulo):
+   print(modulo)
+   try:         
+      x = button.winfo_rootx()
+      y = button.winfo_rooty()
+      popup.tk_popup(x, y, 0)
+   finally:
+      popup.grab_release()
+
+# *********** SECCION DE BOTONES DE LOS DIFERENTES MODULOS ************
+paises = PhotoImage(file="resources/paises.png")
+ciudades = PhotoImage(file="resources/ciudades.png")
+clientes = PhotoImage(file="resources/cliente.png")
+
+btnPaises = Button(master, image=paises, command=popupm)
+btnPaises.configure(command= lambda: popupm(btnPaises, "paises"))
+btnPaises.place(x=30,y=100)
+
+btnCiudades = Button(master, image=ciudades)
+btnCiudades.configure(command= lambda: popupm(btnCiudades, "ciudades"))
+btnCiudades.place(x=188,y=100)
+
+btnClientes = Button(master, image=clientes)
+btnClientes.configure(command= lambda: popupm(btnClientes, "clientes"))
+btnClientes.place(x=346,y=100)
 
 
-        # *********** SECCION DE BOTONES DE LOS DIFERENTES MODULOS ************
-        self.paises = PhotoImage(file="resources/paises.png")
-        self.ciudades = PhotoImage(file="resources/ciudades.png")
-        self.clientes = PhotoImage(file="resources/cliente.png")
-        Button(self.master, image=self.paises, command=self.printHola).place(x=30,y=100)
-        Button(self.master, image=self.ciudades).place(x=188,y=100)
-        Button(self.master, image=self.clientes).place(x=346,y=100)
-
-    def printHola(self):
-        print("Presionaste el boton de Paises")
 
 
-def main():
 
-    root = Tk()
-    root.geometry("880x620")
-    app = MainFrame()
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+master.mainloop()
