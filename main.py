b3cf4a0ffdff95c327c7473e5a287d86d4e0d93f
liangbinjie <mailandresliang@gmail.com>
2023-05-07 00:40:34 -0600

    Update popup botones

--- main.py
+++ main.py
@@ -6,73 +6,91 @@ master = Tk()
 master.geometry("880x620")
 master.title("TecnoPETS - Tu Veterinaria de confianza")
 
-menubar = Menu(master)
-master.config(menu=menubar)
+
+# ******** TOOLBAR ********
+toolbar = Menu(master)
+master.config(menu=toolbar)
 
 
 # definimos la opcion de mantenimiento
-mantenimiento = vMantenimiento(menubar) # la llamamos de la funcion de mantenimiento
+mantenimiento = vMantenimiento(toolbar) # la llamamos de la funcion de mantenimiento
 
 # Definimos la opcion de reportes
-reportes = vReportes(menubar)
+reportes = vReportes(toolbar)
 
-informacion = Menu(menubar)
+facturacion = vFacturacion(toolbar)
+
+informacion = Menu(toolbar)
+informacion.add_cascade(label="TecnoPets, 2023, Cartago Calle IC1803")
+informacion.add_separator()
 informacion.add_cascade(label="Desarrollado por:")
 informacion.add_cascade(label="- Binjie Liang")
 informacion.add_cascade(label="- Aaron Moncada")
 
 
 
-menubar.add_cascade(label="Mantenimiento", underline=0, menu=mantenimiento)
-menubar.add_cascade(label="Reportes", underline=0, menu=reportes)
-menubar.add_cascade(label="Informacion", underline=0, menu=informacion)
-menubar.add_cascade(label="Contacto", underline=0)
+toolbar.add_cascade(label="Mantenimiento", underline=0, menu=mantenimiento)
+toolbar.add_cascade(label="Reportes", underline=0, menu=reportes)
+toolbar.add_cascade(label="Facturacion", underline=0, menu=facturacion)
+toolbar.add_cascade(label="Acerca De", underline=0, menu=informacion)
+toolbar.add_cascade(label="Contacto", underline=0)
 
 
 # ********** SECCION DE REDES SOCIALES Y LOGO ****************
-facebook = PhotoImage(file='resources/facebook.png')
-instagram = PhotoImage(file='resources/instagram.png')
-twitter = PhotoImage(file="resources/twitter.png")
-logo = PhotoImage(file='resources/LOGO.png')
+LOGO_FB = PhotoImage(file='resources/facebook.png')
+LOGO_INS = PhotoImage(file='resources/instagram.png')
+LOGO_TWT = PhotoImage(file="resources/twitter.png")
+LOGO = PhotoImage(file='resources/LOGO.png')
 
-Label(master, image=facebook).place(x=30, y=550)
-Label(master, image=instagram).place(x=90, y=550)
-Label(master, image=twitter).place(x=150, y=550)
-Label(master, image=logo).place(x=560, y=550)
+Label(master, image=LOGO_FB).place(x=30, y=550)
+Label(master, image=LOGO_INS).place(x=90, y=550)
+Label(master, image=LOGO_TWT).place(x=150, y=550)
+Label(master, image=LOGO).place(x=560, y=550)
 
 # ********* POPUPS ***********
 popup = Menu(master, tearoff=0)
-popup.add_command(label="Insertar")
-popup.add_command(label="Consultar")
-popup.add_command(label="Modificar")
-popup.add_command(label="Eliminar")
+popupPaises = Menu(popup)
 
+popupPaises.add_command(label="Insercion")
+popupPaises.add_command(label="Consultar")
+popupPaises.add_command(label="Eliminar")
+popupPaises.add_command(label="Modificar")
+popup.add_cascade(label="Paises", menu=popupPaises)
+
+# ** FUNCION DE POPUP **
 def popupm(button, modulo):
    print(modulo)
    try:         
       x = button.winfo_rootx()
       y = button.winfo_rooty()
-      popup.tk_popup(x, y, 0)
+      popup.tk_popup(x+128, y, 0)
    finally:
       popup.grab_release()
 
 # *********** SECCION DE BOTONES DE LOS DIFERENTES MODULOS ************
-paises = PhotoImage(file="resources/paises.png")
-ciudades = PhotoImage(file="resources/ciudades.png")
+
+lugares = PhotoImage(file="resources/lugares.png")
+btnLugares = Button(master, image=lugares, command=popupm)
+btnLugares.configure(command= lambda: popupm(btnLugares, "paises"))
+btnLugares.place(x=250,y=30)
+Label(text="Lugares", font=("Arial", 20)).place(x=265, y=168)
+
+
 clientes = PhotoImage(file="resources/cliente.png")
-
-btnPaises = Button(master, image=paises, command=popupm)
-btnPaises.configure(command= lambda: popupm(btnPaises, "paises"))
-btnPaises.place(x=30,y=100)
-
-btnCiudades = Button(master, image=ciudades)
-btnCiudades.configure(command= lambda: popupm(btnCiudades, "ciudades"))
-btnCiudades.place(x=188,y=100)
-
 btnClientes = Button(master, image=clientes)
 btnClientes.configure(command= lambda: popupm(btnClientes, "clientes"))
-btnClientes.place(x=346,y=100)
+btnClientes.place(x=450,y=30)
 
+
+clinica = PhotoImage(file="resources/clinica.png")
+btnClinica = Button(master, image=clinica)
+btnClinica.configure(command= lambda: popupm(btnClinica, "clientes"))
+btnClinica.place(x=250,y=300)
+
+facturacion = PhotoImage(file="resources/facturacion.png")
+btnFacturacion = Button(master, image=facturacion)
+btnFacturacion.configure(command= lambda: popupm(btnFacturacion, "clientes"))
+btnFacturacion.place(x=450,y=300)
 
 
 
